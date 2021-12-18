26c8edd64d278342456b571468e771d3325bae82
an6748le <andreas.lemke@fh-erfurt.de>
2021-12-18 22:07:20 +0100

    Added class Campus

---
+++ src/main/java/de/fherfurt/campus/Campus.java
@@ -0,0 +1,12 @@
+package de.fherfurt.campus;
+
+import java.util.Date;
+
+public class Campus {
+    String street;
+    int zip;
+    String feature;
+    String name;
+    String addressNumber;
+    Date openingHours;
+}
