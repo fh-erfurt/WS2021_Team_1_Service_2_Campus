67cd583749906342e9b16f0b52c18faf03e8cad0
an6748le <andreas.lemke@fh-erfurt.de>
2021-12-19 16:46:35 +0100

    Getter and Setter added

--- src/main/java/de/fherfurt/campus/Campus.java
+++ src/main/java/de/fherfurt/campus/Campus.java
@@ -3,10 +3,58 @@ package de.fherfurt.campus;
 import java.util.Date;
 
 public class Campus {
-    String street;
-    int zip;
-    String feature;
-    String name;
-    String addressNumber;
-    Date openingHours;
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
+    public void setFeature(String feature) {
+        this.feature = feature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public Date getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(Date openingHours) {
+        this.openingHours = openingHours;
+    }
+
+   private String street;
+   private int zip;
+   private String feature;
+   private String name;
+   private String addressNumber;
+   private Date openingHours;
 }
