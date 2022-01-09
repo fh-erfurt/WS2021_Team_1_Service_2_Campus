0b998a8b14956803b8c3d5e95b77bbc8f6fa454e
an6748le <andreas.lemke@fh-erfurt.de>
2022-01-09 16:46:24 +0100

    Added Tests and Updated Search class

--- src/main/java/de/fherfurt/campus/Search.java
+++ src/main/java/de/fherfurt/campus/Search.java
@@ -9,20 +9,38 @@ public class Search {
         put("Steinplatz 2", new CampusLocation(2.2,1.2));
         put("Leipziger Straße 77", new CampusLocation(2.2,1.2));
         put("Schlüterstraße 1", new CampusLocation(2.2,1.2));
+        put("Campus nicht gefunden", new CampusLocation(0,0));
     }};
+
 
     public CampusLocation findByStreet(Campus street) {
         switch (street.getStreet()) {
             case "Altonaer Straße":
                 return locations.get("Campus Altonaer Straße");
             case "Steinplatz 2":
-                return locations.get("Campus Steinplatz 2");
+                return locations.get("Steinplatz 2");
             case "Leipziger Straße 77":
-                return locations.get("Campus Leipziger Strße 77");
+                return locations.get("Leipziger Straße 77");
             case "Schlüterstraße 1":
-                return locations.get("Campus Schlüterstraße 1");
+                return locations.get("Schlüterstraße 1");
             default:
                 return locations.get("Campus nicht gefunden");
-        }
+        }}
+
+
+
+        public CampusLocation findByName(String name) {
+
+            CampusLocation res = new CampusLocation(0,0);
+            //For every entry in entry set do compare to the list
+            for (Map.Entry<String, CampusLocation> entry : locations.entrySet()) {
+                if (entry.getKey().toLowerCase().contains(name.toLowerCase())) {
+                    res = entry.getValue();
+                }
+            }
+            return res;
     }
 }
+
+
+
