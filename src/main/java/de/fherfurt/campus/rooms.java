3e3de0aa60180682c5a906cf59fb134fe371b71a
S0RR0R <92719137+S0RR0R@users.noreply.github.com>
2022-01-25 16:45:25 +0100

    Removed console inputs and outputs

---
+++ src/main/java/de/fherfurt/campus/rooms.java
@@ -0,0 +1,39 @@
+package de.fherfurt.campus;
+
+public class rooms {
+
+    public string getBuilding{
+        return building;
+    }
+
+    public void setBuilding(string building){
+        this.building = building;
+    }
+
+
+    public string getFloor{
+        return floor;
+    }
+
+    public void setFloor(string floor){
+        this.floor = floor;
+    }
+
+
+    public string getRoomNumber{
+        return roomNumber;
+    }
+
+    public void setRoomNumber(string roomNumber){
+        this.roomNumber = roomNumber;
+    }
+
+
+    public string getRoomType{
+        return roomType;
+    }
+
+    public void setRoomType(string roomType){
+        this.roomType = roomType;
+    }
+}
