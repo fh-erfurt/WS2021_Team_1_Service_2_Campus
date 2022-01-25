6986e0b773c6fb7b3d870a4362383c15c0efd471
S0RR0R <92719137+S0RR0R@users.noreply.github.com>
2022-01-25 17:28:28 +0100

    Added Enum
    Enum is experimental and may be scrapped later

--- src/main/java/de/fherfurt/campus/Rooms/Rooms.java
+++ src/main/java/de/fherfurt/campus/Rooms/Rooms.java
@@ -2,11 +2,27 @@
 
 
     public class rooms {
-
+        
+        //Basic values to generate the address
         public int building;
         public int floor;
         public int roomNumber;
 
+        //Creates an address for a room based on int values in which the room type will later be saved
         public String[][][] roomAddress = new String[building][floor][roomNumber];
+        
+        //Enum with different types of rooms, May need expansion
+        //Needs refinement
+        public enum roomType {
+            Mensa,
+            Bilbliothek,
+            Vorlesungssaal,
+            Prüfungsraum,
+            Seminarraum,
+            Sekreteriat,
+            Komputerpool,
+            Toilette,
+            Haus- und Gebäudetechnick
+        }
 
     }
