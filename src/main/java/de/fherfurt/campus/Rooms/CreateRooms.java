169aa48a9e83a079a26194817e7259a3d7b389ee
S0RR0R <92719137+S0RR0R@users.noreply.github.com>
2022-01-28 13:57:26 +0100

    Create CreateRooms.java
    still needs room naming

---
+++ src/main/java/de/fherfurt/campus/Rooms/CreateRooms.java
@@ -0,0 +1,28 @@
+package de.fherfurt.campus;
+
+public class createRoom{
+  
+  public int getBuilding(){
+    return building;
+  }
+  
+  public void setBuilding(int building){
+    this.building = building;
+  }
+
+  public int getFloor(){
+    return floor;
+  }
+  
+  public void setFloor(int floor){
+    this.floor = floor;
+  }
+  
+  public int getRoomNumber(){
+    return roomNumber;
+  }
+  
+  public void setRoomNumber(int roomNumber){
+    this.roomNumber = roomNumber;
+  }
+}
