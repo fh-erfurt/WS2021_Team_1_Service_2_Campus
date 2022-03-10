5227f81e6706cc4544ca09f63c16a95610622ec6
Jonas Kruse <jonas.kruse@fh-erfurt.de>
2022-03-10 10:15:10 +0100

    added UnitTests for Building Classes and Sonar Lint

--- src/main/java/de/fherfurt/campus/building/Building.java
+++ src/main/java/de/fherfurt/campus/building/Building.java
@@ -5,22 +5,25 @@ public class Building {
     public int buildingNumber;
     public Unit type;
 
-    public void drawTriangle(Point position, int angle){
+    public char drawTriangle(Point position, int angle){
         /**draws Triangle that points in direction*/
         System.out.println("x: " + position.x + "\ny: " + position.y + "\nAngle: " + angle);
+        return 0;
     }
 
-    public void drawPoint(Point position){
+    public char drawPoint(Point position){
         /**draws Point at position*/
         System.out.println("x: " + position.x + "\ny: "+ position.y);
+        return 0;
     }
 
-    public void drawRectangle(Point p1, Point p2, Point p3, Point p4){
+    public char drawRectangle(Point p1, Point p2, Point p3, Point p4){
         /**calculates vectors between four points and draws them*/
         System.out.println("Edge 1: " + (p1.x - p2.x) + "/" + (p1.y - p2.y) +
                            "Edge 2: " + (p3.x - p2.x) + "/" + (p3.y - p2.y) +
                            "Edge 3: " + (p4.x - p3.x) + "/" + (p4.y - p3.y) +
                            "Edge 4: " + (p4.x - p1.x) + "/" + (p4.y - p1.y));
+        return 0;
     }
 
     public class Entrance{
--- src/test/java/de/fherfurt/campus/building/BuildingTest.java
+++ src/test/java/de/fherfurt/campus/building/BuildingTest.java
@@ -1,6 +1,7 @@
 package de.fherfurt.campus.building;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,13 +19,25 @@ class BuildingTest {
 
     @Test
     void shouldReturnTriangle() {
+        Building newBuilding = new Building();
+        Point newPoint = new Point();
+        newPoint.setX(2);
+        newPoint.setY(4);
+        Assertions.assertEquals("x: 2 y:4 Angle: 45", newBuilding.drawTriangle(newPoint, 45));
+
     }
 
     @Test
     void shouldReturnPoint() {
+        Building newBuilding = new Building();
+        Point newPoint = new Point();
+        newPoint.setX(5);
+        newPoint.setY(3);
+        Assertions.assertEquals("x: 5  y:3 ", newBuilding.drawPoint(newPoint));
     }
 
     @Test
     void shouldReturnRectangle() {
+
     }
 }
