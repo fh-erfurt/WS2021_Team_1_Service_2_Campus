810c5702ef9f28647a3a2e5ba19e69da9f770622
pkebe <peer.ebersoll@fh-erfurt.de>
2022-03-24 19:54:27 +0100

    Ratified enum should now work as intended

--- src/main/java/de/fherfurt/campus/Rooms/CreateRooms.java
+++ src/main/java/de/fherfurt/campus/Rooms/CreateRooms.java
@@ -1,34 +1,50 @@
 package de.fherfurt.campus.Rooms;
 
 public class CreateRooms {
-  
-    public int getBuilding(){
-      return building;
-    }
-  
-  public void setBuilding(int building){
+
+  public int getBuilding() {
+    return building;
+  }
+
+  public void setBuilding(int building) {
     this.building = building;
   }
 
-  public int getFloor(){
+  public int getFloor() {
     return floor;
   }
-  
-  public void setFloor(int floor){
+
+  public void setFloor(int floor) {
     this.floor = floor;
   }
-  
-  public int getRoomNumber(){
+
+  public int getRoomNumber() {
     return roomNumber;
   }
-  
-  public void setRoomNumber(int roomNumber){
+
+  public void setRoomNumber(int roomNumber) {
     this.roomNumber = roomNumber;
   }
-
 
 
   private int building;
   private int floor;
   private int roomNumber;
+
+  /*
+  This Comment is to show how the enum of the package Rooms was intended to be used.
+
+  Scanner roomTypeInput = new Scanner(System.in);
+
+  if (roomTypeCheck.valueOf(roomTypeInput))
+  {
+  roomType = roomTypeInput;
+  System.out.println("Success!");
+  }
+  else
+  {
+  System.out.println("Error! Invalid room type.")
+  }
+   */
 }
+
