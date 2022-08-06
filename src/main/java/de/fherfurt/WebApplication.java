55a0105c8cbdcec92ba495eee8ec4dd984f38575
The_Platypus <zarazach@protonmail.com>
2022-08-06 17:52:30 +0200

    webserver läuft

--- src/main/java/de/fherfurt/WebApplication.java
+++ src/main/java/de/fherfurt/WebApplication.java
@@ -6,39 +6,47 @@ import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.net.URI;
 
 public class WebApplication {
 
     public static final String BASE_URI = "http://localhost:8080/";
 
-    public static Server startServer()
-    {
+    public static Server startServer() {
 
         final ResourceConfig config = new ResourceConfig(BaseResource.class);
-        final Server server = JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
+        final Server server =
+                JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
 
         return server;
+
     }
 
-    public static void main(String[] args){
-        Logger LOG = LoggerFactory.getLogger(WebApplication.class);
+    public static void main(String[] args) {
 
-        try{
+        Logger LOG = LoggerFactory.getLogger( WebApplication.class );
+
+        try {
+
             final Server server = startServer();
 
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                try{
-                    LOG.info("Shutting down");
+                try {
+                    LOG.info("Shutting down the application...");
                     server.stop();
-                    LOG.info("Done");
-                }catch(Exception e){
+                    LOG.info("Done, exit.");
+                } catch (Exception e) {
                     LOG.error(null, e);
                 }
             }));
-            LOG.info("Application started");
+
+            LOG.info("Application started. -- Stop the application using CTRL+C\n");
+
+            // block and wait shut down signal, like CTRL+C
             Thread.currentThread().join();
-        }catch (InterruptedException ex){
+
+        } catch (InterruptedException ex) {
             LOG.error(null, ex);
         }
     }
